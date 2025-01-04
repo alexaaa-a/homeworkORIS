9f1daefc1dde62917abafaec5c769618ceae7cd2
alexaaa-a <144144005+alexaaa-a@users.noreply.github.com>
2025-01-04 19:26:18 +0300

    Delete graphicClient2.py

--- graphicClient2.py
+++
@@ -1,9 +0,0 @@
-from PyQt6.QtWidgets import QApplication
-import sys
-from graphicClient import GraphicClient
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    window = GraphicClient()
-    window.show()
-    sys.exit(app.exec())
